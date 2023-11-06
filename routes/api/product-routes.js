53627774b2875a90ae38588f1a2079816e105066
James Peltier <peltierjames@gmail.com>
2023-11-05 22:20:22 -0800

    product routes updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,15 +5,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      { model: Category },
+      { model: Tag, through: ProductTag },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.status(500).json(err));
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [
+      { model: Category },
+      { model: Tag, through: ProductTag },
+    ],
+  })
+    .then((product) => res.json(product))
+    .catch((err) => res.status(500).json(err));
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -93,7 +107,14 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => res.json({ message: 'Product deleted' }))
+    .catch((err) => res.status(400).json(err));
 });
+
 
 module.exports = router;
